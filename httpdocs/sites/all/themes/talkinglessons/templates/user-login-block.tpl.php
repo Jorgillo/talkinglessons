e9f2e3b827154f1abfd273807e5d0787dcc3703d
deployer <deployer@example.com>
2014-10-27 08:33:04 +0100

    Initial commit

---
+++ httpdocs/sites/all/themes/talkinglessons/templates/user-login-block.tpl.php
@@ -0,0 +1,10 @@
+<div id="user-login-block-container">
+  <div id="user-login-block-form-fields">
+    <?php print $name; // Display username field ?>
+    <?php print $pass; // Display Password field ?>
+    <?php print $submit; // Display submit button ?>
+  </div>
+  <div class="links">
+    <a href="/user/password">Forgot Password</a>
+  </div>
+</div>
